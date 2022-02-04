f2466efc5d484ea60009bdc9474a701972e9cb71
AnniYuko <yuko99@outlook.de>
2022-02-04 19:17:02 +0100

    fixed small mistakes

---
+++ c08/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 20:20:49 by akroll            #+#    #+#             */
+/*   Updated: 2022/02/05 20:20:49 by akroll           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+#define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int size;
+	char *str;
+	char *copy;
+} t_stock_str;
+
+#endif
