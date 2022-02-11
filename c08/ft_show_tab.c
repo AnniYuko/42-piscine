1713eefdcdae5d3636a567ed47cbd4b9d3c74a41
AnniYuko <yuko99@outlook.de>
2022-02-11 20:58:06 +0100

    works as it should

--- c08/ft_show_tab.c
+++ c08/ft_show_tab.c
@@ -10,7 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_stock_str.h"
+#include "../c09/includes/ft.h"
+
 void ft_show_tab(struct s_stock_str *par)
 {
+	int i;
 
+	i = 0;
+	while (par[i].str != NULL)
+	{
+		ft_putstr(par[i].str);
+		write(1, "\n", 1);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstr(par[i].copy);
+		write(1, "\n", 1);
+		i++;
+	}
+	return;
 }
