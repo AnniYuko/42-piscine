abdfd07cb40e66ca1b8cc6301cfdd7874b0c9e8a
AnniYuko <yuko99@outlook.de>
2022-02-14 19:00:44 +0100

    update

--- c12/ft_list.h
+++ c12/ft_list.h
@@ -19,4 +19,8 @@ typedef struct s_list
 	void			*data;
 }	t_list;
 
+void	ft_list_push_front(t_list **begin_list, void *data);
+t_list *ft_create_elem(void *data);
+int	ft_list_size(t_list *begin_list);
+
 #endif
---
+++ c12/main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/13 17:20:51 by akroll            #+#    #+#             */
+/*   Updated: 2022/02/13 17:20:51 by akroll           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include "../c09/includes/ft.h"
+
+int main(void)
+{
+	t_list *elem1;
+	t_list *elem2;
+	t_list *elem3;
+	t_list *begin_list;
+	void *data;
+
+	elem1 = ft_create_elem(data);
+	elem2 = ft_create_elem(data);
+	elem1->next = elem2;
+	elem3 = ft_create_elem(data);
+	elem2->next = elem3;
+
+	begin_list = elem1;
+
+
+	printf("Currently %d element(s) in the list.\n", ft_list_size(begin_list));
+	return(0);
+}
