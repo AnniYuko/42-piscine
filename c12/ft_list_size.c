01ef73549ba55a7bee2bf49c886d34ec020e352a
AnniYuko <yuko99@outlook.de>
2022-02-14 18:59:23 +0100

    passed all tests

---
+++ c12/ft_list_size.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 17:50:02 by akroll            #+#    #+#             */
+/*   Updated: 2022/02/14 17:50:02 by akroll           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include "../c09/includes/ft.h"
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	i;
+
+	i = 1;
+	if (begin_list == NULL)
+		return(0);
+	while (begin_list->next != NULL)
+	{
+		begin_list = begin_list->next;
+		i++;
+	}
+	return(i);
+}
