783333e7ab55e566034ca3f909717c63122be34f
AnniYuko <yuko99@outlook.de>
2022-02-20 22:04:05 +0100

    test for ft_list_reverse.c

--- c12/main.c
+++ c12/main.c
@@ -10,27 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_list.h"
-#include "../c09/includes/ft.h"
+//#include "ft_list.h"
+//#include "liblist.a"
+#include "ft_create_elem.c"
+//#include "ft_list_size.c"
+#include "ft_list_print.c"
+#include "ft_list_reverse.c"
 
 int main(void)
 {
-	t_list *elem1;
-	t_list *elem2;
-	t_list *elem3;
-	t_list **begin_list;
-	void *data;
+	t_list	**begin_list;
+	t_list	*elem1;
+	t_list	*other_elem;
+	void	*data;
+	int		i;
 
+	i = 42;
+	data = &i;
 	elem1 = ft_create_elem(data);
-	elem2 = ft_create_elem(data);
-	elem1->next = elem2;
-	elem3 = ft_create_elem(data);
-	elem2->next = elem3;
+	other_elem = ft_create_elem(data);
+	elem1->next = other_elem;
 
 	begin_list = &elem1;
 
-	printf("Currently %d element(s) in the list.\n", ft_list_size(*begin_list));
+	//printf("Currently %d element(s) in the list.\n", ft_list_size(*begin_list));
+	ft_list_print(*begin_list);
 
-	ft_list_push_back(begin_list, data);
+	ft_list_reverse(begin_list);
+	printf("--- after reversing list: ---\n");
+
+	ft_list_print(*begin_list);
+
 	return(0);
 }
