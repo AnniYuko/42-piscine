c26236a2b092db310acbe6952d9a268570d27c1a
AnniYuko <yuko99@outlook.de>
2022-02-23 20:43:41 +0100

    version 1

---
+++ c12/ft_list_foreach.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 13:36:49 by akroll            #+#    #+#             */
+/*   Updated: 2022/02/21 13:36:49 by akroll           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list != NULL)
+	{
+		(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
+
+// ft_list_foreach applies the function given as argument to each of the list’s elements.
